e66f4efeb37ea26b866754d49846c833b6cef678
Abdulkarim Kanaan <a.kanaan@msn.com>
2018-10-01 16:18:13 +0800

    some changes

--- HelloGitApp/HelloGitApp/Program.cs
+++ HelloGitApp/HelloGitApp/Program.cs
@@ -11,7 +11,7 @@ namespace HelloGitApp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Git");
-            Console.WriteLine("this is SIT");
+            Console.WriteLine("this is SIT2");
         }
     }
 }
